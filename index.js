6d2eb762bfb4609c10c9c3d387b69870e40a8d6a
mainoahmuna <mainoahmuna@gmail.com>
2025-08-13 19:29:06 -0400

    add /players endpoint

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const players = [
+    { id: 1, name: 'Lebron James', team: "Lakers", position: 'SF' },
+    { id: 2, name: 'Stephen Curry', team: 'Warriors', position: 'PG'},
+    { id: 3, name: 'Amen Thompson', team: 'Rockets', position: 'SG'}
+]
+
 app.get('/', (req, res) => {
     res.send('Hello World from Basketball Finance Tracker');
 });
+
+app.get('/players', (req, res) => {
+    res.json(players);
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
