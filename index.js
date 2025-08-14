d3015f76933d0a93024c03154ffa32fdf3a7d6eb
mainoahmuna <mainoahmuna@gmail.com>
2025-08-14 19:05:01 -0400

    simple post endpoint

--- index.js
+++ index.js
@@ -8,12 +8,25 @@ const players = [
     { id: 3, name: 'Amen Thompson', team: 'Rockets', position: 'SG'}
 ]
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World from Basketball Finance Tracker');
 });
 
 app.get('/players', (req, res) => {
     res.json(players);
+})
+
+app.post('/players', (req, res) => {
+    console.log(req.body)
+    const { name, team, position } = req.body;
+    if (!name | !team | !position) {
+        return res.status(404).json({ error: 'Missing required fields'});
+    }
+    const newPlayer = { id: players.length + 1, name, team, position };
+    players.push(newPlayer);
+    res.status(201).json(newPlayer);
 })
 
 app.listen(PORT, () => {
